2fffc554eb7ac3878e0ca86c4911c1a3f82a08f8
Saleem Siddiqui <saleem@saleem-siddiqui.com>
2017-06-21 11:40:58 -0500

    Refactored into a private method (no TDD)

--- src/main/java/noTdd/WordWrap.java
+++ src/main/java/noTdd/WordWrap.java
@@ -1,5 +1,9 @@
 package noTdd;
 
+import java.util.AbstractMap;
+import java.util.Map;
+
+
 public class WordWrap {
    public static String wrap(String text, int width) {
        StringBuffer wrappedText = new StringBuffer(text.length());
@@ -9,16 +13,22 @@ public class WordWrap {
 
    private static void recursiveWrapper(String text, StringBuffer wrappedText, int width) {
        if (text.length() > width) {
-           String toAppend = text.substring(0, width);
-           String remainder = text.substring(width);
-           wrappedText.append(toAppend).append('\n');
-           recursiveWrapper(remainder, wrappedText, width);
+           Map.Entry<String, String> split = split(text, width);
+           wrappedText.append(split.getKey()).append('\n');
+           recursiveWrapper(split.getValue(), wrappedText, width);
        } else {
            wrappedText.append(text);
        }
    }
 
-   public static void main(String[] args) {
-       System.out.println(WordWrap.wrap("this is a long line of text", 4));
+    private static Map.Entry<String, String> split(String text, int width) {
+       String toAppend = text.substring(0, width);
+       String remainder = text.substring(width);
+       return new AbstractMap.SimpleImmutableEntry(toAppend, remainder);
+    }
+
+
+    public static void main(String[] args) {
+       System.out.println(WordWrap.wrap("this is a long line of text", 5));
    }
 }
